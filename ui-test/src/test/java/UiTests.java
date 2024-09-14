ebd76e056d7f99354ba7fc93323f667365eda3c6
Mantaci <mantacifery@gmail.com>
2024-09-14 16:12:31 +0300

    Рабочая версия

---
+++ ui-test/src/test/java/UiTests.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class UiTests {
+
+    @Test
+    public void some_test() {
+        // Test logic here
+    }
+
+    @Test
+    @Tag("Smoke")
+    public void some_test2() {
+        // Test logic here
+    }
+}
